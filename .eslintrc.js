58223c124bc87889a3fda6dd4a24c302ad10485b
Zhigamovsky <zhigamovsky@gmail.com>
2021-01-21 19:48:53 +0200

    Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community'],
+  env: {
+    jest: true,
+  },
+  ignorePatterns: ['node_modules/', 'coverage/', 'android/', 'ios/'],
+};
